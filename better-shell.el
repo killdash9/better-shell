4ee06b8791f7363a19109d9ea6c5ee95ce998d33
killdash9 <killdash9>
2017-02-15 11:20:35 -0700

    updated documentation

--- better-shell.el
+++ better-shell.el
@@ -23,13 +23,41 @@
 
 ;;; Commentary:
 ;; 
-;; This package provides two commands.
+;; This package simplifies shell management and sudo access by providing
+;; the following commands.
 ;; 
-;; better-shell-shell -- cycle through current shells, creating one if
-;; no shell exists.  With prefix arg, pop to or create a shell in
-;; the same directory (and same host) as the current buffer.
+;; `better-shell-shell'
+;; --------------------
 ;; 
-;; better-shell-remote-open -- open a shell on a remote server
+;; Cycle through existing shell buffers, in order of recent use.  If
+;; there are no shells, one is created.
+;; 
+;; With C-u prefix arg, invoke `better-shell-for-current-dir'.
+;; 
+;; `better-shell-for-current-dir'
+;; ------------------------------
+;; 
+;; Bring up a shell on the same host and in the same directory as the
+;; current buffer, choosing an existing shell if possible.  The shell
+;; chosen is guaranteed to be idle (not currently running a command).  It
+;; first looks for an idle shell that is already in the buffer's
+;; directory.  If none is found, it looks for another idle shell on the
+;; same host as the buffer.  If one is found, that shell is selected and
+;; automatically placed into the buffer's directory with a `cd` command.
+;; Otherwise, a new shell is created on the same host and in the same
+;; directory as the buffer.
+;; 
+;; `better-shell-remote-open'
+;; --------------------------
+;; 
+;; Open a shell on a remote server, allowing you to choose from any host
+;; you've previously logged into (uses your ~/.ssh/known_hosts file) or
+;; enter a new host.  With C-u prefix arg, get sudo shell.
+;; 
+;; `better-shell-sudo-here'
+;; --------------------------
+;; 
+;; Reopen the current file, directory, or shell as root.
 
 ;;; Code:
 (require 'cl-lib)
