70c787b981caeef8c5f8012b170eb7b9f167cd13
Russell Black <black.russell@gmail.com>
2019-10-25 11:37:32 -0600

    bumped version number

--- better-shell.el
+++ better-shell.el
@@ -5,7 +5,7 @@
 ;; Keywords: convenience
 ;; URL: https://github.com/killdash9/better-shell
 ;; Created: 1st Mar 2016
-;; Version: 1.2
+;; Version: 1.2.1
 ;; Package-Requires: ((emacs "24.4"))
 
 ;; This program is free software; you can redistribute it and/or modify
